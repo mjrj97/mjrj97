31533753401b55de24492bf2f316b0d3fcc90e1c
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2023-12-02 18:31:23 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { lazy } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('@/pages/home/HomePage'));
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="*" element={<>This page doesn&apos;t exist</>} />
+    </Routes>
+  );
+}
+
+export default App;
